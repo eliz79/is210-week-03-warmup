8d266ed7691e976d84e5b0556484b953b1ac8716
Erica Liz <erica.liz@spsmail.cuny.edu>
2015-02-14 03:08:42 +0000

    Added a multiline docstring.

--- task_09.py
+++ task_09.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""A computer that can build the question.
+
+
+This is a test of adding a multi-line to
+
+
+task_09. Hope it's correct."""
+
 
 THE_ANSWER_TO_EVERYTHING = 42
 
 INFINITE_IMPROBABILITY = 'browning motion'
+
